081877d36cc3920ffe9dad87ab891f43412d7ab9
Sebastian Agudelo <agudelocortess@gmail.com>
2024-04-22 11:51:40 -0500

    new sections

--- src/components/Layouts/Main/Main.jsx
+++ src/components/Layouts/Main/Main.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Section } from '../Section/Section'
+import './Main.css'
 
 export const Main = () => {
   return (
     <main className='main-page'>
-        
+        <Section>
+          <img src="" alt="" />
+        </Section>
+        <Section>
+          <h1></h1>
+          <p></p>
+        </Section>
     </main>
   )
 }
--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Header } from '../../Layouts/Header/Header'
 import { Main } from '../../Layouts/Main/Main'
 
+
 export const Home = () => {
   return (
     <>
